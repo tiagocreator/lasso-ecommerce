c976bf9eea510cf44a15742df0740653f3235e26
tiagocreator <contato@tiagocreator.com>
2023-02-09 12:55:56 +0000

    ✨ Add basic routes configuration

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Pages
+import { Home, Contact } from './pages';
+
+// Components
+import { Header, Footer } from './components'
+
 import './App.scss';
 
 function App() {
-  return <div className='App'></div>;
+  return <>
+    <BrowserRouter>
+      <Header/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>;
+          <Route path='/contact' element={<Contact/>}/>;
+        </Routes>
+      <Footer/>
+    </BrowserRouter>
+  </>;
 }
 
 export default App;
