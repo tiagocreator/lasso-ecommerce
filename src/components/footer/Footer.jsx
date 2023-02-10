8bc11f91da1a502344aef95d29af51f9ddaf7cfa
Tiago Leite <contato@tiagocreator.com>
2023-02-10 00:41:39 -0300

    ✨Update Footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,19 @@
+import { FaGithub } from 'react-icons/fa';
+
 import styles from './Footer.module.scss';
+
+const date = new Date();
+const year = date.getFullYear();
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className={styles.footer}>
+      &copy; {year} Lasso by
+      <a href='https://github.com/tiagocreator' target='_blank' rel='noreferrer'>
+        <FaGithub className={styles['icon']} /> tiagocreator
+      </a>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
