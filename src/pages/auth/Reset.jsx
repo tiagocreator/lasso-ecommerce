1050663fe1a3c9e97a0a577b6b906da62d364d4c
Tiago Leite <contato@tiagocreator.com>
2023-02-11 00:38:40 -0300

    ✨ Create Reset Password page

--- src/pages/auth/Reset.jsx
+++ src/pages/auth/Reset.jsx
@@ -1,5 +1,37 @@
+import { Link } from 'react-router-dom';
+
+import Card from '../../components/card/Card';
+
+import ResetImg from '../../assets/img/reset-password.jpg';
+
+import styles from './Auth.module.scss';
+
 const Reset = () => {
-  return <div>Reset</div>;
+  return (
+    <section className={`container ${styles.auth}`}>
+      <div className={styles.img}>
+        <img src={ResetImg} alt='' width='400' height='auto' />
+      </div>
+      <Card>
+        <div className={styles.form}>
+          <h2>Esqueceu a senha?</h2>
+          <p>Digite o endereço de Email da sua conta e receba um link para criar uma nova senha.</p>
+          <form action=''>
+            <input type='text' placeholder='Endereço de Email' />
+            <button className='--btn --btn-primary --btn-block'>Receber link</button>
+            <div className={styles.links}>
+              <p>
+                <Link to='/login'>Fazer login</Link>
+              </p>
+              <p>
+                <Link to='/register'>Criar conta</Link>
+              </p>
+            </div>
+          </form>
+        </div>
+      </Card>
+    </section>
+  );
 };
 
 export default Reset;
