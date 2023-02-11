2670a4bd1b473a67f45a12b8059e172fbf58f213
Tiago Leite <contato@tiagocreator.com>
2023-02-11 00:14:31 -0300

    🎉Create Register page

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,7 +1,36 @@
+import { Link } from 'react-router-dom';
+
+import Card from '../../components/card/Card';
+
+import RegisterImg from '../../assets/img/register.jpg';
+
 import styles from './Auth.module.scss';
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <section className={`container ${styles.auth}`}>
+      <div className={styles.img}>
+        <img src={RegisterImg} alt='' width='400' height='auto' />
+      </div>
+      <Card>
+        <div className={styles.form}>
+          <h2>Criar conta</h2>
+          <form action=''>
+            <input type='text' placeholder='Endereço de Email' />
+            <input type='password' placeholder='Senha' />
+            <input type='password' placeholder='Confirmar senha' />
+            <button className='--btn --btn-primary --btn-block'>Criar conta</button>
+          </form>
+          <span className={styles.register}>
+            <p>Já possui uma conta?</p>
+            <Link className={styles['account-link']} to='/login'>
+              Login
+            </Link>
+          </span>
+        </div>
+      </Card>
+    </section>
+  );
 };
 
 export default Register;
