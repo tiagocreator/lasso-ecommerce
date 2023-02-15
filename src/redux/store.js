e2120921a5de4122202b395fe72cffea7aa345df
Tiago Leite <contato@tiagocreator.com>
2023-02-15 01:03:01 -0300

    ✨ Add auth user slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import authReducer from './slices/authSlice';
 
-const mainReducer = combineReducers();
+const mainReducer = combineReducers({ auth: authReducer });
 
 const store = configureStore({
   reducer: mainReducer,
