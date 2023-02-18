0ae3c3ae4054c20fc3de6fb19bcf0086935be51b
Tiago Leite <contato@tiagocreator.com>
2023-02-18 18:50:24 -0300

    🎉 Add images data file

---
+++ src/components/carousel/img-data.js
@@ -0,0 +1,27 @@
+import img1 from '../../assets/img/carousel-img-1.jpg';
+import img2 from '../../assets/img/carousel-img-2.jpg';
+import img3 from '../../assets/img/carousel-img-3.jpg';
+import img4 from '../../assets/img/carousel-img-4.jpg';
+
+export const imgData = [
+  {
+    image: img1,
+    title: 'Smartphones Novos',
+    description: 'Parcelados em 12 vezes sem juros.',
+  },
+  {
+    image: img2,
+    title: 'Moda Feminina',
+    description: 'Com até 30% de desconto na primeira compra.',
+  },
+  {
+    image: img3,
+    title: 'Moda Masculina',
+    description: 'As melhores marcas.',
+  },
+  {
+    image: img4,
+    title: 'Sapatos Esportivos',
+    description: 'Com 10% de desconto para pagamentos á vista.',
+  },
+];
