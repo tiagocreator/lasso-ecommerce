0e02aefbe1f8ac640c64c708ed71b8cc9d6d3a84
Tiago Leite <contato@tiagocreator.com>
2023-02-19 12:33:58 -0300

    ✏️ Fix typo in export user email

--- src/redux/slices/authSlice.js
+++ src/redux/slices/authSlice.js
@@ -30,7 +30,7 @@ const authSlice = createSlice({
 
 export const { setActiveUser, removeActiveUser } = authSlice.actions;
 export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
-export const selectEmailn = (state) => state.auth.email;
+export const selectEmail = (state) => state.auth.email;
 export const selectUsername = (state) => state.auth.username;
 export const selectUserId = (state) => state.auth.userId;
 
