88ec9245e0db3c7723bddfe41f05cf8cc2abdaa0
Tiago Leite <contato@tiagocreator.com>
2023-02-20 15:00:42 -0300

    ✨Add Admin page nested routes and content

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,9 +1,32 @@
-import styles from './Admin.module.scss'
+import { Route, Routes } from 'react-router-dom';
+
+import {
+  AdminNavbar,
+  AdminHome,
+  AdminViewProducts,
+  AdminAddProduct,
+  AdminOrders,
+} from '../../components/index';
+
+import styles from './Admin.module.scs s';
 
 const Admin = () => {
   return (
-    <div>Admin</div>
-  )
-}
+    <div className={styles.admin}>
+      <div className={styles.navbar}>
+        <AdminNavbar />
+      </div>
+      <div className={styles.content}>
+        <Routes>
+          <Route path='home' element={<AdminHome />}></Route>
+          <Route path='view-all-products' element={<AdminViewProducts />}></Route>
+          <Route path='add-product' element={<AdminAddProduct />}></Route>
+          <Route path='home' element={<AdminHome />}></Route>
+          <Route path='orders' element={<AdminOrders />}></Route>
+        </Routes>
+      </div>
+    </div>
+  );
+};
 
-export default Admin
+export default Admin;
