fc16053787471ac67be6feb816c5b9c2f412df26
Tiago Leite <contato@tiagocreator.com>
2023-02-20 11:22:20 -0300

    🎨 Fix wrong component name

--- src/components/adminRoute/adminRoute.js
+++ src/components/adminRoute/adminRoute.js
@@ -1,11 +1,11 @@
 import { useSelector } from 'react-redux';
 import { selectEmail } from '../../redux/slices/authSlice';
 
-const useAdminRoute = ({ children }) => {
+const UseAdminRoute = ({ children }) => {
   const userEmail = useSelector(selectEmail);
 
   if (userEmail === process.env.REACT_APP_ADMIN_EMAIL) return children;
   else return null;
 };
 
-export default useAdminRoute;
+export default UseAdminRoute;
