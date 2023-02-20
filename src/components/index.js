0382160655fa8f11ce74782075a6f28ad2fc3195
Tiago Leite <contato@tiagocreator.com>
2023-02-20 15:02:55 -0300

    ✨ Update default export components

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,10 @@
 // Export components
 
-export {default as Header} from './header/Header';
-export {default as Footer} from './footer/Footer';
+export { default as Header } from './header/Header';
+export { default as Footer } from './footer/Footer';
+export { default as UseAdminRoute } from './adminRoute/AdminRoute';
+export { default as AdminHome } from './admin/adminHome/AdminHome';
+export { default as AdminNavbar } from './admin/adminNavbar/AdminNavbar';
+export { default as AdminOrders } from './admin/adminOrders/AdminOrders';
+export { default as AdminViewProducts } from './admin/adminViewProducts/AdminViewProducts';
+export { default as AdminAddProduct } from './admin/adminAddProduct/AdminAddProduct';
