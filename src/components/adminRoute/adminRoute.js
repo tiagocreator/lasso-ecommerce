19462e1e589c901341a4236538eb6459388ce0ce
Tiago Leite <contato@tiagocreator.com>
2023-02-21 11:10:17 -0300

    ✨ Create fallback content for non admin access

--- src/components/adminRoute/adminRoute.js
+++ src/components/adminRoute/adminRoute.js
@@ -1,11 +1,37 @@
 import { useSelector } from 'react-redux';
 import { selectEmail } from '../../redux/slices/authSlice';
 
-const UseAdminRoute = ({ children }) => {
+import { AiOutlineArrowLeft } from 'react-icons/ai';
+import { CgDanger } from 'react-icons/cg';
+import { Link } from 'react-router-dom';
+
+export const UseAdminRoute = ({ children }) => {
+  const userEmail = useSelector(selectEmail);
+
+  if (userEmail === process.env.REACT_APP_ADMIN_EMAIL) return children;
+
+  return (
+    <section style={{ height: '80vh' }}>
+      <div className='container'>
+        <h2>
+          <CgDanger /> Permissão Negada
+        </h2>
+        <p>Essa página só pode ser acessada pelo Administrador</p>
+        <br />
+        <Link to='/'>
+          <button className='--btn --btn-danger --btn-lg'>
+            <AiOutlineArrowLeft />
+            Voltar
+          </button>
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export const UseAdminLink = ({ children }) => {
   const userEmail = useSelector(selectEmail);
 
   if (userEmail === process.env.REACT_APP_ADMIN_EMAIL) return children;
   else return null;
 };
-
-export default UseAdminRoute;
