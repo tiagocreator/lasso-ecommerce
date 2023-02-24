f4a77f8911ff894693892c90c244a9dd96de0c1f
Tiago Leite <contato@tiagocreator.com>
2023-02-23 21:55:48 -0300

    ✨ Add Admin Navbar content

--- src/components/admin/adminNavbar/AdminNavbar.jsx
+++ src/components/admin/adminNavbar/AdminNavbar.jsx
@@ -1,7 +1,47 @@
+import { useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+
+import { selectUserName } from '../../../redux/slices/authSlice';
+
 import styles from './AdminNavbar.module.scss';
 
 const AdminNavbar = () => {
-  return <h1>AdminNavbar</h1>;
+  const userName = useSelector(selectUserName);
+
+  const activeLink = ({ isActive }) => (isActive ? `${styles.active}` : '');
+
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.user}>
+        <h2>Área Admin</h2>
+        <h4>{userName}</h4>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <NavLink className={activeLink} to='/admin/home'>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className={activeLink} to='/admin/view-all-products'>
+              Ver Produtos
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className={activeLink} to='/admin/add-product'>
+              Adicionar Produto
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className={activeLink} to='/admin/orders'>
+              Pedidos
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default AdminNavbar;
