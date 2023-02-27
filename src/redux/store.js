a1ba4ff32fff66bb8cc14c9936e290018536a011
Tiago Leite <contato@tiagocreator.com>
2023-02-26 21:21:00 -0300

    ✨ Add products slice to main reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,15 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { configureStore, combineReducers, getDefaultMiddleware } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
+import productReducer from './slices/productSlice';
 
-const mainReducer = combineReducers({ auth: authReducer });
+const mainReducer = combineReducers({ auth: authReducer, product: productReducer });
 
 const store = configureStore({
   reducer: mainReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export default store;
