9cfd35c589b393b8afb5062667c81fe350e5dc01
Tiago Leite <contato@tiagocreator.com>
2023-03-04 20:08:44 -0300

    🎉 Add filter and search product slice

---
+++ src/redux/slices/filterSlice.js
@@ -0,0 +1,61 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  filteredProducts: [],
+};
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState,
+  reducers: {
+    filterProductsBySearch(state, action) {
+      const { products, search } = action.payload;
+      const searchedProducts = products.filter((product) => {
+        return (
+          product.name.toLowerCase().includes(search.toLowerCase()) ||
+          product.category.toLowerCase().includes(search.toLowerCase())
+        );
+      });
+      state.filteredProducts = searchedProducts;
+    },
+    sortProducts(state, action) {
+      const { products, sort } = action.payload;
+      let sortedProducts = [];
+
+      if (sort === 'latest') {
+        sortedProducts = products;
+      }
+
+      if (sort === 'lowest-price') {
+        sortedProducts = products.slice().sort((a, b) => {
+          return a.price - b.price;
+        });
+      }
+
+      if (sort === 'highest-price') {
+        sortedProducts = products.slice().sort((a, b) => {
+          return b.price - a.price;
+        });
+      }
+
+      if (sort === 'ascending') {
+        sortedProducts = products.slice().sort((a, b) => {
+          return a.name.localeCompare(b.name);
+        });
+      }
+
+      if (sort === 'descending') {
+        sortedProducts = products.slice().sort((a, b) => {
+          return b.name.localeCompare(a.name);
+        });
+      }
+
+      state.filteredProducts = sortedProducts;
+    },
+  },
+});
+
+export const { filterProductsBySearch, sortProducts } = filterSlice.actions;
+export const selectFilteredProducts = (state) => state.filter.filteredProducts;
+
+export default filterSlice.reducer;
