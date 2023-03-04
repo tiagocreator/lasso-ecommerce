3bf56a814e10ebc729847bfb737a12368d1f551a
Tiago Leite <contato@tiagocreator.com>
2023-03-04 20:09:26 -0300

    ✨ Add product filter and search reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,13 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
 import productReducer from './slices/productSlice';
+import filterReducer from './slices/filterSlice';
 
-const mainReducer = combineReducers({ auth: authReducer, product: productReducer });
+const mainReducer = combineReducers({
+  auth: authReducer,
+  product: productReducer,
+  filter: filterReducer,
+});
 
 const store = configureStore({
   reducer: mainReducer,
