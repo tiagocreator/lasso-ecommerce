a3a8bd8d74c5e0e9e9e13caf41f039eeaadc7dc5
Tiago Leite <contato@tiagocreator.com>
2023-03-05 03:12:24 -0300

    🎉 Add products list component

---
+++ src/components/productsContainer/productsList/ProductsList.jsx
@@ -0,0 +1,75 @@
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import {
+  filterProductsBySearch,
+  selectFilteredProducts,
+  sortProducts,
+} from '../../../redux/slices/filterSlice';
+
+import { BsFillGridFill } from 'react-icons/bs';
+import { FaListAlt } from 'react-icons/fa';
+import { ProductItem, SearchBar } from '../../index';
+import styles from './ProductsList.module.scss';
+
+const ProductList = ({ products }) => {
+  const [grid, setGrid] = useState(true);
+  const [search, setSearch] = useState('');
+  const [sort, setSort] = useState('latest');
+  const dispatch = useDispatch();
+  const filteredProducts = useSelector(selectFilteredProducts);
+
+  useEffect(() => {
+    dispatch(sortProducts({ products, sort }));
+  }, [dispatch, sort, products]);
+
+  useEffect(() => {
+    dispatch(filterProductsBySearch({ products, search }));
+  }, [dispatch, search, products]);
+
+  return (
+    <div className={styles['grid-container']} id='products'>
+      <div className={styles.top}>
+        <div className={styles.icons}>
+          <BsFillGridFill size={22} color='var(--dark-orange)' onClick={() => setGrid(true)} />
+          <FaListAlt size={24} color='var(--secondary-blue)' onClick={() => setGrid(false)} />
+          <p>
+            <strong>{filteredProducts.length}</strong> Produtos encontrados.
+          </p>
+        </div>
+        <div>
+          <SearchBar value={search} onChange={(e) => setSearch(e.target.value)} />
+        </div>
+        <div className={styles.sort}>
+          <label>Filtrar:</label>
+          <select value={sort} onChange={(e) => setSort(e.target.value)}>
+            <option value='latest'>Recentes</option>
+            <option value='lowest-price'>Mais Baratos</option>
+            <option value='highest-price'>Mais Caros</option>
+            <option value='ascending'>Crescente</option>
+            <option value='descending'>Decrescente</option>
+          </select>
+        </div>
+      </div>
+      <div className={grid ? `${styles.grid}` : `${styles.list}`}>
+        {products.length === 0 ? (
+          <p>
+            Não encontramos nenhum produto para a sua pesquisa. Remova os filtros e tente novamente.
+          </p>
+        ) : (
+          <>
+            {filteredProducts.map((product) => {
+              return (
+                <div key={product.id}>
+                  <ProductItem {...product} grid={grid} product={product} />
+                </div>
+              );
+            })}
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
