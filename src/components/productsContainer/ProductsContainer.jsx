adf108655cfeef8fd87b2efe25916af5b8fc10db
Tiago Leite <contato@tiagocreator.com>
2023-03-05 03:06:26 -0300

    🎉 Add products main container

---
+++ src/components/productsContainer/ProductsContainer.jsx
@@ -0,0 +1,36 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { selectProducts, storeProducts } from '../../redux/slices/productSlice';
+import useFetchCollection from '../../customHooks/useFetchCollection';
+
+import { ProductsFilter, ProductsList, Spinner } from '../index';
+
+import styles from './ProductsContainer.module.scss';
+
+const Product = () => {
+  const { data, loading } = useFetchCollection('products');
+  const products = useSelector(selectProducts);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(
+      storeProducts({
+        products: data,
+      }),
+    );
+  }, [dispatch, data]);
+
+  return (
+    <section>
+      <div className={`container ${styles.container}`}>
+        <aside className={styles.filter}>{loading ? null : <ProductsFilter />}</aside>
+        <div className={styles.content}>
+          {loading ? <Spinner /> : <ProductsList products={products} />}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Product;
