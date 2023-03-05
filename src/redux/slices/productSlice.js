7d902b72a2eab10c01ea6a5e683e9f32a1e63568
Tiago Leite <contato@tiagocreator.com>
2023-03-05 14:54:38 -0300

    ✨ Add get products price range slice

--- src/redux/slices/productSlice.js
+++ src/redux/slices/productSlice.js
@@ -2,6 +2,8 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   products: [],
+  minPrice: null,
+  maxPrice: null,
 };
 
 const productSlice = createSlice({
@@ -11,10 +13,25 @@ const productSlice = createSlice({
     storeProducts(state, action) {
       state.products = action.payload.products;
     },
+    getProductPriceRange(state, action) {
+      const { products } = action.payload;
+      const pricesArray = [];
+      products.map((product) => {
+        const productPrice = product.price;
+        return pricesArray.push(productPrice);
+      });
+      const max = Math.max(...pricesArray);
+      const min = Math.min(...pricesArray);
+
+      state.maxPrice = max;
+      state.minPrice = min;
+    },
   },
 });
 
-export const { storeProducts } = productSlice.actions;
+export const { storeProducts, getProductPriceRange } = productSlice.actions;
 export const selectProducts = (state) => state.product.products;
+export const selectMinPrice = (state) => state.product.minPrice;
+export const selectMaxPrice = (state) => state.product.maxPrice;
 
 export default productSlice.reducer;
