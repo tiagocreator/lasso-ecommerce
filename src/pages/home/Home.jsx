7facff889e4bdd4413788a1bdc0ec736ab13a5a5
Tiago Leite <contato@tiagocreator.com>
2023-03-05 03:07:57 -0300

    ✨ Add products main container

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,27 @@
+import { useEffect } from 'react';
+import { Carousel, ProductsContainer } from '../../components/index';
+
 import styles from './Home.module.scss';
-import Carousel from '../../components/carousel/Carousel';
 
 const Home = () => {
-  return <Carousel></Carousel>;
+  const url = window.location.href;
+  const jumpToProductsSection = () => {
+    if (url.includes('#products')) {
+      window.scrollTo({ top: 700, behavior: 'smooth' });
+      return;
+    }
+  };
+
+  useEffect(() => {
+    jumpToProductsSection();
+  }, []);
+
+  return (
+    <div>
+      <Carousel />
+      <ProductsContainer />
+    </div>
+  );
 };
 
 export default Home;
