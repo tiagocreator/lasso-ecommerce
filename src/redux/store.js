85ab4bc1b12a72d47a4dafcd3700df0a231a8e92
Tiago Leite <contato@tiagocreator.com>
2023-03-07 10:11:12 -0300

    ✨ Add cart reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
 import productReducer from './slices/productSlice';
 import filterReducer from './slices/filterSlice';
+import cartReducer from './slices/cartSlice';
 
 const mainReducer = combineReducers({
   auth: authReducer,
   product: productReducer,
   filter: filterReducer,
+  cart: cartReducer,
 });
 
 const store = configureStore({
