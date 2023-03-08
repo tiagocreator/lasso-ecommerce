67c59f970e0a7e9a3f9ea583d3fed26c9e3e7f61
Tiago Leite <contato@tiagocreator.com>
2023-03-07 23:11:37 -0300

    ✨ Dispatch add product to cart

--- src/components/productsContainer/productItem/ProductItem.jsx
+++ src/components/productsContainer/productItem/ProductItem.jsx
@@ -1,16 +1,25 @@
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+import { addToCart } from '../../../redux/slices/cartSlice';
 
 import { Card } from '../../index';
 
 import styles from './ProductItem.module.scss';
 
 const ProductItem = ({ product, id, grid, name, price, desc, imgUrl }) => {
+  const dispatch = useDispatch();
+
   const breakText = (text, textLength) => {
     if (text.length > textLength) {
       const brokenText = text.substring(0, textLength).concat('...');
       return brokenText;
     }
     return text;
+  };
+
+  const addProductToCart = (product) => {
+    dispatch(addToCart(product));
   };
 
   return (
@@ -26,7 +35,9 @@ const ProductItem = ({ product, id, grid, name, price, desc, imgUrl }) => {
           <h4>{breakText(name, grid ? 18 : 28)}</h4>
         </div>
         {!grid && <p className={styles.desc}>{breakText(desc, 200)}</p>}
-        <button className='--btn --btn-danger'>Adicionar Ao Carrinho</button>
+        <button className='--btn --btn-danger' onClick={() => addProductToCart(product)}>
+          Adicionar Ao Carrinho
+        </button>
       </div>
     </Card>
   );
