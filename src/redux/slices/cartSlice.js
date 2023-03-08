422bcc1692346edaada9799ba09444b7cf684e34
Tiago Leite <contato@tiagocreator.com>
2023-03-07 23:10:16 -0300

    ✨ Add product to localStorage

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -1,5 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+import { toast } from 'react-toastify';
+
 const initialState = {
   cartItems: localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [],
   cartTotalQuantity: 0,
@@ -11,7 +13,20 @@ const cartSlice = createSlice({
   initialState,
   reducers: {
     addToCart(state, action) {
-      console.log(action.payload);
+      const productIndex = state.cartItems.findIndex((item) => item.id === action.payload.id);
+
+      if (productIndex >= 0) {
+        state.cartItems[productIndex].cartTotalQuantity += 1;
+        toast.info(`Adicionado mais um ${action.payload.name} ao carrinho.`, {
+          position: 'top-left',
+        });
+      } else {
+        const productToBeAdded = { ...action.payload, cartTotalQuantity: 1 };
+        state.cartItems.push(productToBeAdded);
+        toast.success(`${action.payload.name} adicionado ao carrinho!`, { position: 'top-left' });
+      }
+
+      localStorage.setItem('cartItems', JSON.stringify(state.cartItems));
     },
   },
 });
