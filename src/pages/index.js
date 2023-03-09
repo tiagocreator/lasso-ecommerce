f79d650fe38078802e6b3a82711373ed5f265a34
Tiago Leite <contato@tiagocreator.com>
2023-03-09 00:40:20 -0300

    ✨ Update default pages exports

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,3 +6,4 @@ export { default as Login } from './auth/Login';
 export { default as Register } from './auth/Register';
 export { default as Reset } from './auth/Reset';
 export { default as Admin } from './admin/Admin';
+export { default as Cart } from './cart/Cart';
