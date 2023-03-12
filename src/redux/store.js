74f8618143dd97cddb4aab07c357d1590788fd32
Tiago Leite <contato@tiagocreator.com>
2023-03-12 18:50:51 -0300

    ✨ Add checkout slice to main reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,12 +3,14 @@ import authReducer from './slices/authSlice';
 import productReducer from './slices/productSlice';
 import filterReducer from './slices/filterSlice';
 import cartReducer from './slices/cartSlice';
+import checkoutReducer from './slices/checkoutSlice';
 
 const mainReducer = combineReducers({
   auth: authReducer,
   product: productReducer,
   filter: filterReducer,
   cart: cartReducer,
+  checkout: checkoutReducer,
 });
 
 const store = configureStore({
