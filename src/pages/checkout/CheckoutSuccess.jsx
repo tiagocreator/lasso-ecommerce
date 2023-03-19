9d3b78c45ad7b08aa612e599d8143ded033b0907
Tiago Leite <contato@tiagocreator.com>
2023-03-19 10:06:49 -0300

    🎉 Create checkout success page

--- src/pages/checkout/CheckoutSuccess.jsx
+++ src/pages/checkout/CheckoutSuccess.jsx
@@ -1,5 +1,21 @@
+import { Link } from 'react-router-dom';
+
 const CheckoutSuccess = () => {
-  return <div>CheckoutSuccess</div>;
+  return (
+    <section>
+      <div className='container'>
+        <h2>Pagamento efetuado com sucesso!</h2>
+        <p>
+          Parabéns pela sua compra! Seu pedido está sendo processado, e será embalado e enviado o
+          mais rápido possível.
+        </p>
+        <br />
+        <Link to='/order-history'>
+          <button className='--btn --btn-primary'>Ver Status Do Pedido</button>
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default CheckoutSuccess;
