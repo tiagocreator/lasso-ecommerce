0ef894877ef5c5ce310de04fa3eb800bff8dee4d
Tiago Leite <contato@tiagocreator.com>
2023-03-25 08:33:32 -0300

    ✏️ Fix typo

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const calculateOrderAmount = (items) => {
   return totalProductsPriceAmount * 100;
 };
 
-if (process.env.REACT_APP_NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static('build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
