3c1427bcf63e11d7aa005df182e6b216084bac46
Tiago Leite <contato@tiagocreator.com>
2023-03-29 23:07:34 -0300

    ✨ Add firebase logic to product review
    Fetch product from collection and send review to a 'review' collection

--- src/components/reviewProduct/ReviewProduct.jsx
+++ src/components/reviewProduct/ReviewProduct.jsx
@@ -1,41 +1,79 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import StarsRating from 'react-star-rate';
 
-import { selectProducts } from '../../redux/slices/productSlice';
+import { addDoc, collection, Timestamp } from 'firebase/firestore';
+import { db } from '../../firebase/config';
+import { selectUserId, selectUserName } from '../../redux/slices/authSlice';
 
+import useFetchDocument from '../../customHooks/useFetchDocument';
+
+import { toast } from 'react-toastify';
 import { Card } from '../index';
+import spinnerGif from '../../assets/img/spinner.gif';
 
 import styles from './ReviewProduct.module.scss';
 
 const ReviewProduct = () => {
   const [rate, setRate] = useState(0);
   const [review, setReview] = useState('');
+  const [fetchProduct, setFetchProduct] = useState(null);
   const { id } = useParams();
-  const products = useSelector(selectProducts);
+  const userId = useSelector(selectUserId);
+  const userName = useSelector(selectUserName);
+  const { document } = useFetchDocument('products', id);
 
-  const product = products.find((item) => item.id === id);
+  useEffect(() => {
+    setFetchProduct(document);
+  }, [document]);
 
   const submitReview = (e) => {
     e.preventDefault();
 
-    console.log(rate, review);
+    const getDate = new Date();
+    const dateString = `${getDate.getDate()}/${getDate.getMonth() + 1}/${getDate.getFullYear()}`;
+
+    const reviewConfig = {
+      userId,
+      userName,
+      productId: id,
+      rate,
+      review,
+      reviewDate: dateString,
+      uploadTime: Timestamp.now().toDate(),
+    };
+
+    try {
+      addDoc(collection(db, 'reviews'), reviewConfig);
+      toast.success('Parabéns, você deixou a sua avaliação do produto!');
+      setRate(0);
+      setReview('');
+    } catch (e) {
+      toast.error('Erro ao fazer avaliação do produto. Tente novamente mais tarde.');
+    }
   };
 
   return (
     <section>
       <div className={`container ${styles.container}`}>
         <h2>Avaliar Produto</h2>
-        <p>
-          <strong>Produto: </strong>
-          {product.name}
-        </p>
-        <img
-          style={{ width: '200px', marginBlock: '30px' }}
-          src={product.imgUrl}
-          alt={product.name}
-        />
+        {fetchProduct === null ? (
+          <img style={{ width: '70px' }} src={spinnerGif} alt='Carregando' />
+        ) : (
+          <>
+            <p>
+              <strong>Produto: </strong>
+              {fetchProduct.name}
+            </p>
+            <img
+              style={{ width: '200px', marginBlock: '30px' }}
+              src={fetchProduct.imgUrl}
+              alt={fetchProduct.name}
+            />
+          </>
+        )}
+
         <Card cardClass={styles.card}>
           <form onSubmit={(e) => submitReview(e)}>
             <label>Estrelas:</label>
