c5b1e83117f3d70c75af9e210a1aca025716a015
Tiago Leite <contato@tiagocreator.com>
2023-04-04 22:08:05 -0300

    ✨ 🚧 Add contact page content

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,7 +1,66 @@
 import styles from './Contact.module.scss';
 
+import { Card } from '../../components/index';
+import { FaEnvelope, FaPhoneAlt, FaTwitter } from 'react-icons/fa';
+import { GoLocation } from 'react-icons/go';
+
 const Contact = () => {
-  return <div>Contact</div>;
+  const submitContactForm = (e) => {
+    e.preventDefault();
+    console.log('Enviado!');
+  };
+
+  return (
+    <section>
+      <div className={`container ${styles.container}`}>
+        <h2>Entre em Contato</h2>
+        <div className={styles.section}>
+          <form onSubmit={(e) => submitContactForm(e)}>
+            <Card cardClass={styles.card}>
+              <label>Nome Completo:</label>
+              <input type='text' name='contact-user-name' placeholder='Nome Completo' required />
+              <label>Email:</label>
+              <input
+                type='email'
+                name='contact-user-email'
+                placeholder='Seu Melhor Email'
+                required
+              />
+              <label>Motivo do Contato:</label>
+              <input type='text' name='contact-subject' placeholder='Motivo do Contato' required />
+              <label>Mensagem:</label>
+              <textarea name='' cols='30' rows='10' placeholder='Mensagem' required></textarea>
+              <button className='--btn --btn-primary'>Entrar em Contato</button>
+            </Card>
+          </form>
+          <div className={styles.details}>
+            <Card cardClass={styles.detailsCard}>
+              <h3>Informações de Contato e Endereço</h3>
+              <p>Além do formulário, você também pode entrar em contato pelos seguintes meios:</p>
+              <div className={styles.icons}>
+                <span>
+                  <FaPhoneAlt />
+                  <p>+55 12 3456789</p>
+                </span>
+                <span>
+                  <FaEnvelope />
+                  <p>contato@lasso.com</p>
+                </span>
+                <span>
+                  <GoLocation />
+                  <p>Maceió, AL</p>
+                </span>
+                <span>
+                  <FaTwitter />
+                  <p>@lasso</p>
+                </span>
+              </div>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
