029e7757a024c4620c63f054611f9b02e9bc731b
Tiago Leite <contato@tiagocreator.com>
2023-08-04 20:15:51 -0300

    ✨ Create mobile admin navbar

--- src/components/admin/adminNavbar/AdminNavbar.jsx
+++ src/components/admin/adminNavbar/AdminNavbar.jsx
@@ -1,11 +1,14 @@
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+
+import { CgArrowUpR, CgArrowDownR } from 'react-icons/cg';
 
 import { selectUserName } from '../../../redux/slices/authSlice';
 
 import styles from './AdminNavbar.module.scss';
 
-const AdminNavbar = () => {
+export const AdminNavbar = () => {
   const userName = useSelector(selectUserName);
 
   const activeLink = ({ isActive }) => (isActive ? `${styles.active}` : '');
@@ -39,4 +42,50 @@ const AdminNavbar = () => {
   );
 };
 
-export default AdminNavbar;
+export const MobileAdminNavbar = () => {
+  const userName = useSelector(selectUserName);
+
+  const [toggleMobileNavBar, setToggleMobileNavBar] = useState(false);
+
+  const activeLink = ({ isActive }) => (isActive ? `${styles.active}` : '');
+
+  const mobileNavBarHandler = () => {
+    setToggleMobileNavBar(!toggleMobileNavBar);
+  };
+
+  return (
+    <div className={styles['mobile-navbar']}>
+      <h2>Menu Admin</h2>
+      <nav className={toggleMobileNavBar ? `${styles.active}` : ''}>
+        <div></div>
+        <ul>
+          <li onClick={() => setToggleMobileNavBar(false)}>
+            <NavLink className={activeLink} to='/admin/view-all-products'>
+              Ver Produtos
+            </NavLink>
+          </li>
+          <li onClick={() => setToggleMobileNavBar(false)}>
+            <NavLink className={activeLink} to='/admin/add-product/new'>
+              Adicionar Produto
+            </NavLink>
+          </li>
+          <li onClick={() => setToggleMobileNavBar(false)}>
+            <NavLink className={activeLink} to='/admin/orders'>
+              Pedidos
+            </NavLink>
+          </li>
+          <li>
+            <p>Usuário: {userName}</p>
+          </li>
+        </ul>
+      </nav>
+      <div className={styles['mobile-navbar-toggler']} onClick={mobileNavBarHandler}>
+        {!toggleMobileNavBar ? (
+          <CgArrowUpR />
+        ) : (
+          <CgArrowDownR style={{ color: 'var(--dark-orange)' }} />
+        )}
+      </div>
+    </div>
+  );
+};
